1b4a93fe760d8bb20932bbd619552160072396ed
wolf2lyon <josesebastiancu@gmail.com>
2024-08-10 19:10:00 -0500

    finished proyect

---
+++ src/data/CardInfoData.js
@@ -0,0 +1,26 @@
+export const cardInfo = [
+    {
+        title:'Supervisor',
+        body:'Monitors activity to identify project roadblocks',
+        src:'/assets/icons/icon-supervisor.svg',
+        name_image:'icon-supervisor'
+    },
+    {
+        title:'Team Builder',
+        body:'Scans our talent network to create the optimal team for your project',
+        src:'/assets/icons/icon-team-builder.svg',
+        name_image:'icon-team-builder'
+    },
+    {
+        title:'Karma',
+        body:'Regularly evaluates our talent to ensure quality',
+        src:'/assets/icons/icon-karma.svg',
+        name_image:'icon-karma'
+    },
+    {
+        title:'Calculator',
+        body:'Uses data from past projects to provide better delivery estimates',
+        src:'/assets/icons/icon-calculator.svg',
+        name_image:'icon-calculator'
+    }
+]
---
+++ src/data/HeaderInfoData.js
@@ -0,0 +1,5 @@
+export const headerInfo = {
+        titulo_normal:'Reliable, efficient delivery',
+        titulo_strong:'Powered by Technology',
+        description:'Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful' 
+}
